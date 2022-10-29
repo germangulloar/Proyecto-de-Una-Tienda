8fe29e9b539e4c3c881e22ee69a4feb097c1fb86
germangulloar <113535960+germangulloar@users.noreply.github.com>
2022-10-29 18:21:32 -0500

    Termine la parte que me corresponde a la historia de usurio 1 y 2 aqui les mando lo que llevo hasta el moemnto estoy en el video del miercoles del profe

--- app/src/main/java/com/example/proyecto/ToDoFragment.kt
+++ app/src/main/java/com/example/proyecto/ToDoFragment.kt
@@ -12,7 +12,6 @@ class ToDoFragment : Fragment() {
     {
         super.onCreate(savedInstanceState)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,7 +26,7 @@ class ToDoFragment : Fragment() {
         val btnDtail6:Button=fragmento.findViewById(R.id.btn_detail_6)
         btnDtail1.setOnClickListener(View.OnClickListener {
             val datos= Bundle()
-            datos.putString("Tarea","Elementos de Aseo")
+            datos.putString("Tarea","llevar Elementos de Aseo a la mascota")
             datos.putString("hora","8:00A.M.")
             datos.putString("lugar","Norte de Bogota")
             activity?.getSupportFragmentManager()?.beginTransaction()
@@ -35,8 +34,64 @@ class ToDoFragment : Fragment() {
                 ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail1")
                 ?.addToBackStack( "")
                 ?.commit()
-
         })
+        btnDtail2.setOnClickListener(View.OnClickListener {
+            val datos= Bundle()
+            datos.putString("Tarea","llevar las vitaminas Vitaminas a la mascota")
+            datos.putString("hora","9:00A.M.")
+            datos.putString("lugar","Norte de Bogota Colina Campestre")
+            activity?.getSupportFragmentManager()?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail1")
+                ?.addToBackStack( "")
+                ?.commit()
+        })
+        btnDtail3.setOnClickListener(View.OnClickListener {
+            val datos= Bundle()
+            datos.putString("Tarea","llevar comida a la mascota")
+            datos.putString("hora","10:00A.M.")
+            datos.putString("lugar","Norte de Bogota barrio Colina Campestre")
+            activity?.getSupportFragmentManager()?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail1")
+                ?.addToBackStack( "")
+                ?.commit()
+        })
+        btnDtail4.setOnClickListener(View.OnClickListener {
+            val datos= Bundle()
+            datos.putString("Tarea","llevar los accesorios a la mascota")
+            datos.putString("hora","11:00A.M.")
+            datos.putString("lugar","Norte de Bogota barrio Colina Campestre")
+            activity?.getSupportFragmentManager()?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail1")
+                ?.addToBackStack( "")
+                ?.commit()
+        })
+        btnDtail5.setOnClickListener(View.OnClickListener {
+            val datos= Bundle()
+            datos.putString("Tarea","Asistir con un veterinario para la mascota")
+            datos.putString("hora","12:00P.M.")
+            datos.putString("lugar","Norte de Bogota barrio Colina Campestre")
+            activity?.getSupportFragmentManager()?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail1")
+                ?.addToBackStack( "")
+                ?.commit()
+        })
+        btnDtail6.setOnClickListener(View.OnClickListener {
+            val datos= Bundle()
+            datos.putString("Tarea"," Contactarse con asesor sobre la mascota")
+            datos.putString("hora","1:00P.M.")
+            datos.putString("lugar","Norte de Bogota barrio Colina Campestre")
+            activity?.getSupportFragmentManager()?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.fcv,DetailFragment::class.java,datos,"detail1")
+                ?.addToBackStack( "")
+                ?.commit()
+        })
+
         return fragmento
-    }
+        }
+
 }
