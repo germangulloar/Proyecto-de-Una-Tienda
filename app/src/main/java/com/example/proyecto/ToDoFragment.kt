26d8607e49194111d644d105acd9dcacf5bdda7d
germangulloar <113535960+germangulloar@users.noreply.github.com>
2022-10-30 16:01:08 -0500

    subo lo que faltaba pero falta es corregir algo que no esta desplegando el menu del ultimo video de la clase antes de hacer la configuracion del romm

--- app/src/main/java/com/example/proyecto/ToDoFragment.kt
+++ app/src/main/java/com/example/proyecto/ToDoFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 
 class ToDoFragment : Fragment() {
+    private lateinit var listRecyclerView: RecyclerView
+    private lateinit var myAdapter: RecyclerView.Adapter<MyTaskListAdapter.MyViewHolder>
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -18,7 +24,7 @@ class ToDoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val fragmento = inflater.inflate(R.layout.fragment_to_do,container,false)
-        val btnDtail1:Button=fragmento.findViewById(R.id.btn_detail_1)
+        /*val btnDtail1:Button=fragmento.findViewById(R.id.btn_detail_1)
         val btnDtail2:Button=fragmento.findViewById(R.id.btn_detail_2)
         val btnDtail3:Button=fragmento.findViewById(R.id.btn_detail_3)
         val btnDtail4:Button=fragmento.findViewById(R.id.btn_detail_4)
@@ -90,8 +96,41 @@ class ToDoFragment : Fragment() {
                 ?.addToBackStack( "")
                 ?.commit()
         })
-
+*/
         return fragmento
         }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var myTaskTitles: ArrayList<String> = ArrayList()
+        var myTaskTimes: ArrayList<String> = ArrayList()
+        var myTaskPlaces: ArrayList<String> = ArrayList()
+        myTaskTitles.add("elementos de aseo")
+        myTaskTitles.add("vitaminas")
+        myTaskTitles.add("comida")
+        myTaskTitles.add("accesorios para tu peludito")
+        myTaskTitles.add("contactar un veterinario")
+        myTaskTitles.add("contactar con un asesor")
+        myTaskTimes.add("8:00AM")
+        myTaskTimes.add("9:00AM")
+        myTaskTimes.add("10:00AM")
+        myTaskTimes.add("11:00AM")
+        myTaskTimes.add("12:00PM")
+        myTaskTimes.add("1:00PM")
+        myTaskPlaces.add("Bogota Colina Campestre")
+        myTaskPlaces.add("Bogota Colina Campestre")
+        myTaskPlaces.add("Bogota Colina Campestre")
+        myTaskPlaces.add("Bogota Colina Campestre")
+        myTaskPlaces.add("Bogota Colina Campestre")
+        myTaskPlaces.add("Bogota Colina Campestre")
+        var info: Bundle= Bundle()
+        info.putStringArrayList("titles",myTaskTitles)
+        info.putStringArrayList("times",myTaskTimes)
+        info.putStringArrayList("places",myTaskPlaces)
+        listRecyclerView = requireView().findViewById(R.id.recyclerToDoList)
+        myAdapter = MyTaskListAdapter(activity as AppCompatActivity,info)
+        listRecyclerView.setHasFixedSize(true)
+        listRecyclerView.adapter = myAdapter
+        listRecyclerView.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+    }
 }
